8022bec8be35c89588ec1f15559a88d48846cdd3
Abhishek Biswal <ab@ab.(none)>
2013-10-24 13:31:32 +0530

    changed db url

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ from app import app
 from models import db
 
 app.secret_key = 'abhishekbiswal123'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://abhishekbiswal:idontknowpassword@localhost/namanyayg_clients_rostergram'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:abhishekbiswal@localhost/rg'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://abhishekbiswal:idontknowpassword@localhost/namanyayg_clients_rostergram'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:abhishekbiswal@localhost/rg'
 db.init_app(app)

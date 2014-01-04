717e439789fbb93e976a2336fc245d1ba697a86a
Namanyay Goel <mail@namanyayg.com>
2014-01-04 17:42:19 +0530

    Fixing JS

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,28 +1,35 @@
 $(document).ready(function(){
 
 	$(".team-select input").change(function(){
-       var value = this.value;
-       //alert(value)
-       if(this.checked)
-       {
-         //alert(value)
-         $(".team"+value).slideDown();
-       }
-       else
-       {
-         $(".team"+value).slideUp();
-       }
-     })
+    hideShowTeams(this);
+  });
+    
+  //   
 
 	$('.js--all-teams').click(function(e) {
     e.preventDefault();
+
 		$(".team-select input").each(function() {
 			$(this).prop('checked', true);
 		})
 
 		$('.team').each(function() {
-			$(this).slideUp();
-		})
+			$(this).slideDown();
+		});
+
 	});
+
+  //
+
+  function hideShowTeams(input) {
+    var value = input.value;
+    var isChecked = input.checked;
+
+    if(isChecked) {
+     $(".team"+value).slideDown();
+    } else {
+     $(".team"+value).slideUp();
+    }
+  } 
 
 });

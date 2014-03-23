2c9590f835ec9b353600c4d164c80cbf34292ba1
Namanyay Goel <mail@namanyayg.com>
2014-03-23 15:41:21 +0530

    Styling fixes + messing with the script

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -18,6 +18,8 @@ $(document).ready(function(){
       var label = encodeURIComponent($(this).find('span').html().replace(/\s+/g, '').replace('&', '-and-'));
       var historyState = History.getState().data;
 
+      console.log(label);
+
       if ( jQuery.inArray( label, historyState ) !== -1 ) {
         $(this).prev().prop('checked', true);
       } else {
@@ -79,8 +81,10 @@ $(document).ready(function(){
     var labels = [];
 
     $('.team-select input:checked').each(function() {
-      labels.push(encodeURIComponent($(this).next('span').html().replace(/\s+/g, '').replace('&', '-and-')));
+      var label = encodeURIComponent($(this).next('span').html().replace(/\s+/g, '').replace('&', '-and-'));
+      labels.push(label);
       // finds labels next to inputs, and removes whitespace, and encodes to URL
+      console.log(label + ' (checkURL)');
     });
 
     History.pushState(labels, 'RosterGram', '?t=' + labels.join('+'));
